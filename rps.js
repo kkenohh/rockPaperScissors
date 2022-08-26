ec23b4a0b1d980808c13cfe8f1981d328669ef02
Ken Oh <kenoh17@uw.edu>
2022-08-25 22:42:53 -0700

    finally finished

--- rps.js
+++ rps.js
@@ -1,3 +1,38 @@
+// main text selector
+const mainText = document.querySelector('#main-text');
+
+// icon selector
+const playerSelect = document.querySelector('.player.icon');
+const computerSelect = document.querySelector('.computer.icon');
+
+// button selector
+const buttons = document.querySelectorAll('.button');
+
+buttons.forEach( button => {
+    button.addEventListener('click', playRound)
+});
+
+// score selector
+const playerScore = document.querySelector('.player.points');
+const computerScore = document.querySelector('.computer.points');
+
+// name selector
+const playerName = document.querySelector('.player.name');
+const computerName = document.querySelector('.computer.name');
+
+// reset selector
+const reset = document.querySelector('#reset');
+reset.addEventListener('click', resetGame);
+
+// global variables
+const options = {
+    '0': 'Rock',
+    '1': 'Paper',
+    '2': 'Scissors'
+}
+let playerCount = 0;
+let comCount = 0;
+
 /*
     capitalizes the first letter of the given
     string and lowercases the rest
@@ -20,14 +55,32 @@ function getComputerChoice() {
     let choice = getRandomInt(3);
     switch (choice) {
         case 0:
-            choice = "Rock";
+            computerSelect.innerHTML = '<i class="icon fa-regular fa-hand-back-fist fa-8x"></i>';
             break;
         case 1: 
-            choice = "Paper";
+            computerSelect.innerHTML = '<i class="icon fa-regular fa-hand fa-8x"></i>';
             break;
         case 2:
-            choice = "Scissors";
+            computerSelect.innerHTML = '<i class="icon fa-regular fa-hand-scissors fa-8x"></i>';
             break;
+    }
+    return choice;
+}
+
+function getPlayerChoice(word) {
+    const rockRegex = new RegExp('rock.+');
+    const paperRegex = new RegExp('paper.+');
+    const scissorsRegex = new RegExp('scissors.+');
+    let choice;
+    if (word.match(rockRegex)) {
+        playerSelect.innerHTML = '<i class="icon fa-regular fa-hand-back-fist fa-8x"></i>';
+        choice = 0;
+    } else if (word.match(paperRegex)) {
+        playerSelect.innerHTML = '<i class="icon fa-regular fa-hand fa-8x"></i>';
+        choice = 1;
+    } else if (word.match(scissorsRegex)) {
+        playerSelect.innerHTML = '<i class="icon fa-regular fa-hand-scissors fa-8x"></i>';
+        choice = 2;
     }
     return choice;
 }
@@ -36,82 +89,64 @@ function getComputerChoice() {
     compares the users choice to the computers to decide who won the round
     or if it was a tie
 */
-function playRound(player, computer) {
-    if ((player == "Rock" && computer == "Paper") || 
-            (player == "Paper" && computer == "Scissors") || 
-            (player == "Scissors" && computer == "Rock")) {
-                
-        return 0;
+function playRound(e) {
+    let player = getPlayerChoice(e.target.className);
+    let computer = getComputerChoice();
+    let playerConvert = options[player + ''];
+    let comConvert = options[computer + ''];
 
-    } else if ((computer == "Rock" && player == "Paper") || 
-            (computer == "Paper" && player == "Scissors") || 
-            (computer == "Scissors" && player == "Rock")) {
-                
-        return 1;
-
-    } else {
-
-        return 2;
-
-    }
-}
-
-let playerCount = 0;
-let comCount = 0;
-const playerScore = document.querySelector('.player');
-const computerScore = document.querySelector('.computer');
-
-/* 
-    plays a game of rock, paper, scissors, best of 5
-*/
-function game() {
-    let player;
-    let computer;
-    playerCount = 0;
-    comCount = 0;
-    playerScore.textContent = `${playerCount}`;
-    computerScore.textContent = `${comCount}`;
-    alert("You will be playing a game of Rock, Paper, Scissors, best of 5. Are you ready?")
-
-    for (let i = 0; i < 5; i++) {
-        player = capitalize(prompt("Rock, Paper, or Scissors? "));
-        computer = getComputerChoice();
-        let result = playRound(player, computer);
-
-        let path = helper(result, player, computer);
-
-        while (path == 2) {
-            player = capitalize(prompt("It was a tie or you suck at spelling. Go again. Rock, Paper, or Scissors? "));
-            computer = getComputerChoice();
-            result = playRound(player, computer);
-            path = helper(result, player, computer);
-        }
-        
-        if (path == 0) comCount++;
-        playerCount += path;
-        playerScore.textContent = `${playerCount}`;
+    if ((player == 0 && computer == 1) || 
+        (player == 1 && computer == 2) || 
+        (player == 2 && computer == 0)) {
+            
+        comCount++;
         computerScore.textContent = `${comCount}`;
-        if (playerCount === 3 || comCount === 3) break;
-    }
-
-    playerCount >= 3 ? console.log("You Win! Computer says: well played") : console.log("You Lose! Computer says: gg ez ur trash");
-}
-
-/* 
-    helper function for game()
-*/
-function helper(result, player, computer) {
-    if (result == 0) {
-        console.log(`You Lose! ${computer} beats ${player}`);
-        return 0;
-    } else if (result == 1) {
-        console.log(`You Win! ${player} beats ${computer}`);
-        return 1;
+        computerName.classList.remove('red');
+        playerName.classList.remove('green');
+        computerName.classList.add('green');
+        playerName.classList.add('red');
+        if (comCount === 5) {
+            mainText.textContent = 'You lost! Hit "Reset Game" to play again!';
+        } else {
+            mainText.textContent = `You lost this round. ${comConvert} beats ${playerConvert}`;
+        }
+    } else if ((computer == 0 && player == 1) || 
+            (computer == 1 && player == 2) || 
+            (computer == 2 && player == 0)) {
+                
+        playerCount++;
+        playerScore.textContent = `${playerCount}`;
+        computerName.classList.remove('green');
+        playerName.classList.remove('red');
+        computerName.classList.add('red');
+        playerName.classList.add('green');
+        if (playerCount === 5) {
+            mainText.textContent = 'You won! Hit "Reset Game" to play again!';
+        } else {
+            mainText.textContent = `You won this round. ${playerConvert} beats ${comConvert}`;
+        }
     } else {
-        return 2;
+
+        mainText.textContent = "Y'all tied. Go again.";
+
     }
+    if (playerCount == 5 || comCount == 5) {
+        buttons.forEach(button => button.disabled = true);
+    }
+    
 }
 
-
-
-
+function resetGame() {
+    mainText.innerHTML = 'Rock, Paper, Scissors. First to 5. <span class="tertiary">Ready?</span>';
+    playerSelect.innerHTML = '';
+    computerSelect.innerHTML = '';
+    playerScore.textContent = '0';
+    computerScore.textContent = '0';
+    comCount = 0;
+    playerCount = 0;
+    buttons.forEach(button => button.disabled = false);
+    computerName.classList.remove('green');
+    playerName.classList.remove('red');
+    computerName.classList.remove('red');
+    playerName.classList.remove('green');
+}
